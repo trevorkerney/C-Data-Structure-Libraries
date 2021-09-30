170e020871a3be1a47002b01f6adf0c128fcc027
Evosity <trevorkerney@hotmail.com>
2021-09-30 01:35:37 -0500

    successfully tested sorting user created objects

--- TVector/tests/indev/main.cpp
+++ TVector/tests/indev/main.cpp
@@ -2,25 +2,64 @@
 
 #include <iostream>
 
+class obj
+{
+private:
+    int id;
+public:
+    obj() {}
+    obj(int p_id)
+    {
+        id = p_id;
+    }
+
+    int get_id() const
+    {
+        return id;
+    }
+};
+
+template<>
+inline int TVector<obj>::compare(const obj& p_obj1, const obj& p_obj2) const
+{
+    int comparison = p_obj1.get_id() - p_obj2.get_id();
+    
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+
 int main()
 {
-    TVector<std::string> tvs;
+    obj obj1(3);
+    obj obj2(5);
+    obj obj3(8);
+    obj obj4(11);
+    obj obj5(17);
+
+    obj objs[5];
+    int nums[5];
+
+    TVector<obj> tvs;
 
     tvs.reserve(5);
 
-    tvs.push("Indiana Jones");
-    tvs.push("Inception");
-    tvs.push("Star Wars");
-    tvs.push("Scarface");
-    tvs.push("Heat");
-    tvs.push("Hardball");
-    tvs.push("Harry Potter");
+    tvs.push(obj3);
+    tvs.push(obj4);
+    tvs.push(obj2);
+    tvs.push(obj1);
+
+    tvs.insert(obj5, 0);
 
     tvs.sort();
 
     for (int _i = 0; _i < tvs.size(); _i++)
     {
-        std::cout << tvs[_i] << std::endl;
+        std::cout << tvs[_i].get_id() << std::endl;
     }
 
     std::cout << "size: " << tvs.size() << std::endl;
