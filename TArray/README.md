87ab7775f2550cdcd77efae75ede0349955e2c5a
trevorkerney <trevorkerney@hotmail.com>
2021-10-06 03:53:16 -0500

    updated TArray and its readme and makefile

--- TArray/README.md
+++ TArray/README.md
@@ -1,50 +1,111 @@
 # TArray
 
-This implementation is incomplete.
-
 My C++ Array implementation.
 
 Positioning: None.
 
 Memory format: Sequential.
 
-Maximum size (MAX_SIZE) of TArray is 65535.
+Maximum capacity (MAX_CAPACITY) of TArray is 65535.
+
+### Searching Algorithms
+- linear
+
+The use of find() with non-[fundamental types](https://www.cplusplus.com/reference/type_traits/is_fundamental/) requires explicit specialization of the private compare() method. The template is as follows:
+```
+explspec short TArray<myType>::compare(const myType& obj1, const myType& obj2) const
+{
+    int comparison =    /* DO COMPARISON HERE */;
+
+                /* COMPARISON SHOULD BE POSITIVE IF obj1 IS LARGER */
+    
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+```
+This must be implemented after the inclusion of TVector and before the use of any function that uses searching/sorting methods.
+
+Replace all instances of "myType" with the name of the type/class.
+
+*explspec* is a #define that expands to "template<>". You don't have to use it, but I think it improves readability.
+
+## Public Methods:
+
+(c): constructor
+
+(d): destructor
+
+(=): assignment operator
+
+(o): non-assignment operator
 
 ---
+### TArray(int p_initial_capacity = 0); (c)
 
-## Member Functions:
-### TVector(const unsigned int& initial_size = 2); (constructor)
-
-Allocates array on the heap.
+Initializes the array.
 
 parameters:
-- initial_size: the memory capacity initally allocated for the list. The default is 2. Throws error if less than 2 or greater than MAX_SIZE.
+- initial_size: the memory capacity initally allocated for the list. The default is 0. Throws error if less than 0 or greater than the maximum capacity.
 
-### ~TArray(); (destructor)
+---
+### TArray(const TArray& p_other); (c)
 
-Deallocates array.
+Initializes the array and copies another array's contents to itself.
 
-### type& at(const int& index) const;
+parameters:
+- other: the TArray to copy.
+
+---
+### ~TArray(); (d)
+
+Deallocates the memory allocated to the array, if any.
+
+---
+### TArray& operator=(const TArray& p_other); (=)
+
+Copies another array's contents into itself.
+
+parameters:
+- other: the TArray to copy
+
+---
+### type& at(const long& index) const;
 
 Returns value at index. Throws error if index is out of range.
 
 parameters:
-- index: the index at which the vector is accessed.
+- index: the index at which the array is accessed.
 
-### type& operator[](const int& index) const;
+---
+### type& operator[](const long& index) const; (o)
 
-Returns at(index);
-
-parameters:
-- index: the index at which the vector is accessed.
-
-### unsigned int size() const;
-
-Returns current amount of objects in vector.
-
-### unsigned int reserve(const int& amount);
-
-Manually expands the vector's capacity. Returns amount of new spaces in vector's capacity. Trying to reserve more than the vector's maximum capacity does not throw an error, but simply reserves the maximum capacity. Any call to reserve when vector's capacity is at maximum size will immediately return 0.
+Returns value at index. Throws error if index is out of range.
 
 parameters:
-- amount: the new capacity of the vector.
+- index: the index at which the array is accessed.
+
+---
+### int capacity() const;
+
+Returns the capacity of the array.
+
+---
+### unsigned int reserve(const long& amount);
+
+Manually expands the array's capacity. Returns the amount of new spaces in the array's capacity. Trying to reserve more than the array's maximum capacity does not throw an error, but simply reserves the maximum capacity. Any call to reserve when the array's capacity is at maximum capacity will immediately return 0.
+
+parameters:
+- amount: the new capacity of the array.
+
+---
+### long find(const type& searched) const;
+
+Finds and returns the value referred to by searched.
+
+parameters:
+- searched: the object to find.
--- TArray/inc/TArray.h
+++ TArray/inc/TArray.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#define explspec template<>
+
+#include <string>
+using std::string;
+using std::to_string;
+
 #include <stdexcept>
+
+#include <cmath>
+using std::pow;
 
 template <typename type>
 class TArray
@@ -9,46 +18,87 @@ private:
 
     type* i_array;
 
-    unsigned int i_size;
-    void inc_size(int p_amount = 1)
+    unsigned int i_capacity;
+
+    static const unsigned int MAX_CAPACITY = 65535;
+
+    void copy(const TArray& p_other)
     {
-        i_size += p_amount;
+        if (p_other.i_capacity > 0)
+        {
+            if (i_capacity > 0)
+            {
+                delete[] i_array;
+            }
+            i_array = new type[p_other.i_capacity];
+
+            for (unsigned int _i = 0; _i < p_other.i_capacity; _i++)
+            {
+                i_array[_i] = p_other[_i];;
+            }
+        }
+        i_capacity = p_other.i_capacity;
     }
 
-    static const unsigned int MAX_SIZE = 65535;
+    inline short compare(const type& p_obj1, const type& p_obj2) const
+    {
+        throw std::runtime_error("There is no comparison function provided for this type.");
+    }
+
+    unsigned int linear_search(const type& p_searched) const
+    {
+        for (unsigned int _i = 0; _i < i_capacity; _i++)
+        {
+            int comparison = compare(p_searched, i_array[_i]);
+            if ((comparison) == 0)
+                return _i;
+        }
+        return -1;
+    }
 
 public:
 
-    TArray(int p_initial_size = 2)
+    TArray(int p_initial_capacity = 0)
     {
-        if (p_initial_size >= 2 && p_initial_size < MAX_SIZE)
+        if (p_initial_capacity >= 0 && p_initial_capacity < MAX_CAPACITY)
         {
-            i_array = new type[p_initial_size];
-            i_size = p_initial_size;
+            i_capacity = 0;
+            if (p_initial_capacity > 0)
+            {
+                reserve(p_initial_capacity);
+            }
         }
         else
         {
-            throw std::runtime_error("Out of allowed TArray range: 0 - " + MAX_SIZE);
+            throw std::runtime_error("Out of allowed TArray range: 0 - " + to_string(MAX_CAPACITY));
         }
+    }
+    TArray(const TArray& p_other)
+    {
+        i_capacity = 0;
+        copy(p_other);
     }
     ~TArray()
     {
-        delete i_array;
+        if (i_capacity > 0)
+        {
+            delete[] i_array;
+            i_array = nullptr;
+        }
     }
 
-    type& operator[](int p_index)
+    TArray& operator=(const TArray& p_other)
     {
-        return at(p_index);
+        if (this != &p_other)
+        {
+            copy(p_other);
+        }    
+        return *this;
     }
 
-    int size() const
+    type& at(int p_index) const
     {
-        return i_size;
-    }
-
-    type& at(int p_index) const noexcept(false)
-    {
-        if (!(p_index >= 0 && p_index < size()))
+        if (!(p_index >= 0 && p_index < i_capacity))
         {
             throw std::runtime_error("Out of range");
         }
@@ -59,22 +109,283 @@ public:
         return at(p_index);
     }
 
-    void reserve(int p_amount) noexcept(false)
+    int capacity() const
     {
-        if (p_amount > size() && p_amount <= MAX_SIZE)
+        return i_capacity;
+    }
+
+    unsigned int reserve(const long& p_amount)
+    {
+        unsigned int old_memory_size = i_capacity;
+
+        if ((p_amount == MAX_CAPACITY && i_capacity == MAX_CAPACITY) || p_amount <= i_capacity)
+        {
+            return 0;
+        }
+        else if (p_amount <= MAX_CAPACITY)
         {
             type* new_array = new type[p_amount];
-            for (unsigned int _i = 0; _i < size(); _i++)
+            if (i_capacity > 0)
             {
-                new_array[_i] = i_array[p_amount];
-                i_array = new_array;
+                for (unsigned int _i = 0; _i < i_capacity; _i++)
+                {
+                    new_array[_i] = i_array[_i];
+                }
+                delete[] i_array;
             }
-            inc_size(p_amount - size());
+            i_array = new_array;
+            i_capacity = p_amount;
+            return p_amount - old_memory_size;
         }
         else
         {
-            throw std::runtime_error("Out of range");
+            return reserve(MAX_CAPACITY);
         }
     }
 
+    long find(const type& p_searched) const
+    {
+        return linear_search(p_searched);
+    }
 };
+
+explspec short TArray<bool>::                  compare(const bool& p_obj1,                 const bool& p_obj2)                 const
+{
+    int comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<char>::                  compare(const char& p_obj1,                 const char& p_obj2)                 const
+{
+    short comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<char16_t>::              compare(const char16_t& p_obj1,             const char16_t& p_obj2)             const
+{
+    short comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<char32_t>::              compare(const char32_t& p_obj1,             const char32_t& p_obj2)             const
+{
+    short comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<wchar_t>::               compare(const wchar_t& p_obj1,              const wchar_t& p_obj2)              const
+{
+    short comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<signed char>::           compare(const signed char& p_obj1,          const signed char& p_obj2)          const
+{
+    int comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<short>::                 compare(const short& p_obj1,                const short& p_obj2)                const
+{
+    short comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<int>::                   compare(const int& p_obj1,                  const int& p_obj2)                  const
+{
+    int comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<long>::                  compare(const long& p_obj1,                 const long& p_obj2)                 const
+{
+    long comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<long long>::             compare(const long long& p_obj1,            const long long& p_obj2)            const
+{
+    long long comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<unsigned char>::         compare(const unsigned char& p_obj1,        const unsigned char& p_obj2)        const
+{
+    int comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<unsigned short>::        compare(const unsigned short& p_obj1,       const unsigned short& p_obj2)       const
+{
+    int comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<unsigned>::              compare(const unsigned& p_obj1,             const unsigned& p_obj2)             const
+{
+    int comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<unsigned long>::         compare(const unsigned long& p_obj1,        const unsigned long& p_obj2)        const
+{
+    long comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<unsigned long long>::    compare(const unsigned long long& p_obj1,   const unsigned long long& p_obj2)   const
+{
+    unsigned long long num1 = p_obj1;
+    int num1_digits = 1;
+    while (num1 /= 10)
+        num1_digits++;
+
+    unsigned long long num2 = p_obj2;
+    int num2_digits = 1;
+    while (num2 /= 10)
+        num2_digits++;
+
+    if (num1_digits > num2_digits)
+    {
+        return 1;
+    }
+    else if (num1_digits < num2_digits)
+    {
+        return -1;
+    }
+    else
+    {
+        unsigned short highest_digit = 0;
+        for (int _i = 0; _i < num1_digits; _i++)
+        {
+            unsigned long long current_power = pow(10, num1_digits) - _i;
+            unsigned short num1_digit = (p_obj1 - p_obj1 % current_power) / current_power;
+            unsigned short num2_digit = (p_obj2 - p_obj2 % current_power) / current_power;
+            if (num1_digit > num2_digit)
+            {
+                return 1;
+            }
+            else if (num1_digit < num2_digit)
+            {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    unsigned short comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<float>::                 compare(const float& p_obj1,                const float& p_obj2)                const
+{
+    float comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > (float)0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<double>::                compare(const double& p_obj1,               const double& p_obj2)               const
+{
+    double comparison = p_obj1 - p_obj2;
+    if (comparison != 0)
+        if (comparison > (double)0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
+explspec short TArray<long double>::           compare(const long double& p_obj1,          const long double& p_obj2)          const
+{
+    return 0;
+}
+explspec short TArray<string>::                compare(const string& p_obj1,               const string& p_obj2)               const
+{
+    int comparison = p_obj1.compare(p_obj2);
+    if (comparison != 0)
+        if (comparison > 0)
+            return 1;
+        else
+            return -1;
+    else
+        return 0;
+}
