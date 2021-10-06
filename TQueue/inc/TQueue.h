e96122e1efe31a7da2fcca0f89499dcbdde8e8e6
trevorkerney <trevorkerney@hotmail.com>
2021-10-06 04:21:00 -0500

    updated current state of TQueue

--- TQueue/inc/TQueue.h
+++ TQueue/inc/TQueue.h
@@ -23,35 +23,26 @@ public:
 
     }
 
-
-    // return value held by node
     type value() const 
     {
         return i_value;
     }
-    // returns pointer to node before this one
     TQueueNode<type>* prev() const
     {
         return i_prev;
     }
-    // returns pointer to node after this one
     TQueueNode<type>* next() const 
     {
         return i_next;
     }
-
-
-    // sets prev pointer to parameter
     void set_prev(TQueueNode* p_prev)
     {
         i_prev = p_prev;
     }
-    // sets next pointer to parameter
     void set_next(TQueueNode* p_next)
     {
         i_next = p_next;
     }
-
 };
 
 
@@ -62,30 +53,9 @@ class TQueue
 private:
 
     TQueueNode<type>* i_head = nullptr;
-    // sets head pointer to parameter
-    void set_head(TQueueNode<type>* p_head)
-    {
-        i_head = p_head;
-    }
-    
     TQueueNode<type>* i_foot = nullptr;
-    // sets foot pointer to parameter
-    void set_foot(TQueueNode<type>* p_foot) 
-    {
-        i_foot = p_foot;
-    }
 
     int i_size = 0;
-    // increments i_size
-    void inc_size() 
-    {
-        i_size++;
-    }
-    // decrements i_size
-    void dec_size() 
-    {
-        i_size--;
-    }
 
 public:
 
@@ -104,16 +74,12 @@ public:
         }
     }
 
-
-    // returns size of queue
     int size() const
     {
         return i_size;
     }
 
-
-    // inserts object at end of queue
-    void push_back(type p_obj)
+    void push(type p_obj)
     {
         TQueueNode<type>* node = new TQueueNode<type>(p_obj);
 
@@ -121,17 +87,17 @@ public:
         {
             i_foot -> set_next(node);
             node -> set_prev(i_foot);
-            set_foot(node);
+            i_foot = node;
         }
         else
         {
             set_head(node);
             set_foot(node);
         }
-        inc_size();
+        i_size++;
     }
-    // removes the head of the queue and returns its value
-    type next(int p_quiet = 0) noexcept(false)
+
+    type next()
     {
         type value;
         if (i_head)
